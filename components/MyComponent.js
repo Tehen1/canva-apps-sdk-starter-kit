f7fb555aaf4b28830a507a7606a3f8b42332a166
Tehen1 <tehen.antony@protonmail.com>
2023-08-22 13:45:39 +0200

    !

---
+++ components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AlignTextLeft from "../assets/icons/align-text-left.svg"; // Assurez-vous que le chemin est correct
+
+const MyComponent = () => {
+  return (
+    <div>
+      <img src={AlignTextLeft} alt="Align Text Left Icon" />
+    </div>
+  );
+};
+
+export default MyComponent;
